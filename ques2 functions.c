c4b7357cccda36dd862a51a79564daf1ddc1d209
Srishti <srishtiaggarwal12345@gmail.com>
2023-03-28 12:21:25 +0530

    added second file

---
+++ ques2 functions.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int subtraction(int num1,int num2)
+{
+    int difference;
+    difference=num1-num2;
+    return difference;
+
+}
+int main()
+{
+    int var1,var2;
+    printf("enter two numbers: ");
+    scanf("%d%d",&var1,&var2);
+    int difference=subtraction(var1,var2);
+    printf("output=%d",difference);
+    return 0;
+}
